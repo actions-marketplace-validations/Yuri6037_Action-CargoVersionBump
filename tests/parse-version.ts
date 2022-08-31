4ac626841d73b8b1b67b89d358c30e342bc179c6
Yuri Edward <yuri6037@outlook.com>
2022-08-31 22:12:48 +0200

    Renamed tests to better fit naming convention

--- tests/parse-version.ts
+++ tests/parse-version.ts
@@ -1,7 +1,7 @@
 import { parseVersion } from '../src/utils'
 import { expect, test } from '@jest/globals'
 
-test('parseVersion_simple_1', async () => {
+test('parse_version_simple_1', async () => {
     const version = parseVersion('1.0.0')
     version.bumpMajor()
     version.bumpMinor()
@@ -9,63 +9,63 @@ test('parseVersion_simple_1', async () => {
     expect(version.format()).toEqual('2.1.1')
 })
 
-test('parseVersion_pre_major', async () => {
+test('parse_version_pre_major', async () => {
     const version = parseVersion('1.0.0-pre.0.0.1')
     version.jumpChannel('pre')
     version.bumpMajor()
     expect(version.format()).toEqual('1.0.0-pre.1.0.1')
 })
 
-test('parseVersion_pre_minor', async () => {
+test('parse_version_pre_minor', async () => {
     const version = parseVersion('1.0.0-pre.0.0.1')
     version.jumpChannel('pre')
     version.bumpMinor()
     expect(version.format()).toEqual('1.0.0-pre.0.1.1')
 })
 
-test('parseVersion_pre_patch', async () => {
+test('parse_version_pre_patch', async () => {
     const version = parseVersion('1.0.0-pre.0.0.1')
     version.jumpChannel('pre')
     version.bumpPatch()
     expect(version.format()).toEqual('1.0.0-pre.0.0.2')
 })
 
-test('parseVersion_jump_pre_major', async () => {
+test('parse_version_jump_pre_major', async () => {
     const version = parseVersion('1.0.0')
     version.jumpChannel('pre')
     version.bumpMajor()
     expect(version.format()).toEqual('2.0.0-pre.1.0.0')
 })
 
-test('parseVersion_jump_pre_minor', async () => {
+test('parse_version_jump_pre_minor', async () => {
     const version = parseVersion('1.0.0')
     version.jumpChannel('pre')
     version.bumpMinor()
     expect(version.format()).toEqual('1.1.0-pre.0.1.0')
 })
 
-test('parseVersion_jump_pre_patch', async () => {
+test('parse_version_jump_pre_patch', async () => {
     const version = parseVersion('1.0.0')
     version.jumpChannel('pre')
     version.bumpPatch()
     expect(version.format()).toEqual('1.0.1-pre.0.0.1')
 })
 
-test('parseVersion_jump_major', async () => {
+test('parse_version_jump_major', async () => {
     const version = parseVersion('2.0.0-pre.1.0.0')
     version.jumpChannel(null)
     version.bumpMajor()
     expect(version.format()).toEqual('2.0.0')
 })
 
-test('parseVersion_jump_minor', async () => {
+test('parse_version_jump_minor', async () => {
     const version = parseVersion('1.1.0-pre.0.1.0')
     version.jumpChannel(null)
     version.bumpMinor()
     expect(version.format()).toEqual('1.1.0')
 })
 
-test('parseVersion_jump_patch', async () => {
+test('parse_version_jump_patch', async () => {
     const version = parseVersion('1.0.1-pre.0.0.1')
     version.jumpChannel(null)
     version.bumpPatch()
